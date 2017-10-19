5f0718d9388f042c095184c1deb0d32e7ad1e9d6
nickcirone <32112777+nickcirone@users.noreply.github.com>
2017-10-19 15:52:38 -0400

    init commit 2

---
+++ WebScrape/src/webscrape/SinglePageSoup.java
@@ -0,0 +1,27 @@
+package webscrape;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/*
+ * Class to extract data from a single webpage. Written by Nicholas Cirone,
+ * 10/19/17 - For Industry Hub - diversitypolicy.com
+ */
+
+public class SinglePageSoup {
+
+  public static void main(String[] args) throws IOException {
+    // TODO Auto-generated method stub
+    Document doc = Jsoup.connect("http://diversity.unc.edu/resources/best/").timeout(6000)
+        .get();
+    Elements elements = doc.select("div.panel-body");
+    for (Element currentDiv : elements) {
+
+    }
+  }
+
+}
