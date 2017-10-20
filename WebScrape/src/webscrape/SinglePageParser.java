4faa4d8c9b852b33650397220a9ddad38f0a2106
nickcirone <32112777+nickcirone@users.noreply.github.com>
2017-10-20 19:56:34 -0400

    push 10/20/17
    simple update

--- WebScrape/src/webscrape/SinglePageParser.java
+++ WebScrape/src/webscrape/SinglePageParser.java
@@ -1,12 +1,20 @@
 package webscrape;
 
 import java.io.IOException;
+
+
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+/*
+ * ih-webscraper
+ * Parses a single url and returns ArrayList<String>'s of web page's text data using method specified tags 
+ * Nicholas Cirone for IndustryHub - diversitypolicy.com
+ */
 
 public class SinglePageParser {
 	
@@ -22,6 +30,8 @@ public class SinglePageParser {
 		return _pageUrl;
 	}
 	
+	// Methods to return ArrayLists of List Items : with varying degrees of specificity 
+	
 	public ArrayList<String> getListItems() {
 		Elements rawListItems = _webDoc.select("li");
 		ArrayList<String> liArrList = new ArrayList<String>();
@@ -32,7 +42,7 @@ public class SinglePageParser {
 	    return liArrList;
 	}
 	
-	//Specify the markup tags from which you want to extract li's
+	// Specify the markup tags from which you want to extract li's
 	
 	public ArrayList<String> getListItems(String tag_specify) {
 		Elements specifiedElements = _webDoc.select(tag_specify);
@@ -43,6 +53,115 @@ public class SinglePageParser {
 	    	liArrList.add(listItem);
 	    }
 	    return liArrList;
+	}
+	
+	// double specify 
+	
+	public ArrayList<String> getListItems(String tag_specify1, String tag_specify2) {
+		Elements specifiedElements1 = _webDoc.select(tag_specify1);
+		Elements specifiedElements2 = specifiedElements1.select(tag_specify2);
+		Elements rawListItems = specifiedElements2.select("li");
+		ArrayList<String> liArrList = new ArrayList<String>();
+	    for (Element currentLi : rawListItems) {
+	    	String listItem = currentLi.text();
+	    	liArrList.add(listItem);
+	    }
+	    return liArrList;
+	}
+	
+	// Methods to return ArrayLists of paragraphs : with varying degrees of specificity
+	
+	public ArrayList<String> getParagraphs() {
+		Elements rawParagraphs = _webDoc.select("p");
+		ArrayList<String> paraArrList = new ArrayList<String>();
+		for (Element currentP : rawParagraphs) {
+			String paraItem = currentP.text();
+			paraArrList.add(paraItem);
+		}
+		return paraArrList;
+	}
+	
+	// single specify
+	
+	public ArrayList<String> getParagraphs(String tag_specify) {
+		Elements specifiedElements = _webDoc.select(tag_specify);
+		Elements rawParagraphs = specifiedElements.select("p");
+		ArrayList<String> paraArrList = new ArrayList<String>();
+		for (Element currentP : rawParagraphs) {
+			String paraItem = currentP.text();
+			paraArrList.add(paraItem);
+		}
+		return paraArrList;
+	}
+	
+	// double specify 
+	
+	public ArrayList<String> getParagraphs(String tag_specify1, String tag_specify2) {
+		Elements specifiedElements = _webDoc.select(tag_specify1);
+		Elements specifiedElements2 = specifiedElements.select(tag_specify2);
+		Elements rawParagraphs = specifiedElements2.select("p");
+		ArrayList<String> paraArrList = new ArrayList<String>();
+		for (Element currentP : rawParagraphs) {
+			String paraItem = currentP.text();
+			paraArrList.add(paraItem);
+		}
+		return paraArrList;
+	}
+	
+	// Returns an ArrayList of all links on current webpage : if the link begins with http
+	
+	public ArrayList<String> getLinks() {
+		Elements rawLinks = _webDoc.select("a");
+		ArrayList<String> linkArrList = new ArrayList<String>();
+		for (Element currentLink : rawLinks) {
+			String linkText = currentLink.attr("href");
+			String linkTextSub = linkText.substring(0, 4);
+			if (linkTextSub.equals("http")) {
+				linkArrList.add(linkText);
+			}			
+		}
+		return linkArrList;
+	}
+	
+	// Returns an ArrayList of all headers on current webpage
+	
+	public ArrayList<String> getHeaders() {
+		ArrayList<String> headArrList = new ArrayList<String>();
+		for (int i = 1; i <= 6; i++) {
+			Elements rawHeaders = _webDoc.select("h" + i);	
+			for (Element currentHeader : rawHeaders) {
+				String headText = currentHeader.text();
+				headArrList.add(headText);
+			}
+		}		
+		return headArrList;
+	}
+	
+	// Returns headers of certain weight, i.e. h2 has weight 2, h3 has weight 3 and so on
+	// Returns null ArrayList if weight is < 1 or > 6
+	
+	public ArrayList<String> getHeaders(int weight) {
+		ArrayList<String> headArrList = new ArrayList<String>();
+		if (weight >= 1 && weight <= 6) {
+			Elements rawHeaders = _webDoc.select("h" + weight);		
+			for (Element currentHeader : rawHeaders) {
+				String headText = currentHeader.text();
+				headArrList.add(headText);
+			}
+		}	
+		return headArrList;
+	}
+	
+	// Returns an ArrayList of the current webpage's title
+	
+	public ArrayList<String> getTitle() {
+		Elements rawTitles = _webDoc.select("title");
+		ArrayList<String> titleArrList = new ArrayList<String>();
+		for (Element currentTitle : rawTitles) {
+			String titleText = currentTitle.text();
+			titleArrList.add(titleText);
+		}
+		return titleArrList;
 	}
 	
 }
--- WebScrape/src/webscrape/SoupMain.java
+++ WebScrape/src/webscrape/SoupMain.java
@@ -10,8 +10,8 @@ import org.jsoup.select.Elements;
 
 /*
  * ih-webscraper
- * Class to extract data from a single webpage. Written by Nicholas Cirone,
- * 10/19/17 - For Industry Hub - diversitypolicy.com
+ * Main class for webscraper. Written by Nicholas Cirone,
+ * 10/19/17 - For IndustryHub - diversitypolicy.com
  */
 
 public class SoupMain {
@@ -19,11 +19,21 @@ public class SoupMain {
   public static void main(String[] args) throws IOException {
 	  
     SinglePageParser currentPage = new SinglePageParser("http://diversity.unc.edu/resources/best/");
+    SinglePageParser secondPage = new SinglePageParser("http://diversity.arizona.edu/diversity-policies-statements");
     ArrayList<String> listItems = currentPage.getListItems("div.panel-body");
+    ArrayList<String> headItems = currentPage.getTitle();
+    ArrayList<String> paraItems = secondPage.getParagraphs();
     for (String curr: listItems) {
     	System.out.println(curr);
     }
-    
+    System.out.println();
+    for (String curr: headItems) {
+    	System.out.println(curr);
+    }
+    System.out.println();
+    for (String curr: paraItems) {
+    	System.out.println(curr);
+    }
   }
 
 }
